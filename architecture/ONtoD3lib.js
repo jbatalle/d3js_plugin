dfd36dcfd6f13cb7eb37863c08fc7dab5662f5d4
Josep <jbatalle3@gmail.com>
2014-10-02 10:51:55 +0200

    Useful functions

--- architecture/ONtoD3lib.js
+++ architecture/ONtoD3lib.js
@@ -73,3 +73,50 @@ function addJavascript(jsname, pos) {
     s.setAttribute('src',jsname);
     th.appendChild(s);
 }
+
+function ip2long(ip){
+    var components;
+    var regex = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
+    if(components = ip.match(regex)){
+        var iplong = 0;
+        var power  = 1;
+        for(var i=4; i>=1; i-=1){
+            iplong += power * parseInt(components[i]);
+            power  *= 256;
+        }
+        return iplong;
+    }
+    else return -1;
+}
+
+function inSubNet(ip, subnet){
+    var mask, base_ip, long_ip = ip2long(ip);
+    var regex = /^(.*?)\/(\d{1,2})$/;
+    if( (mask = subnet.match(regex)) && ((base_ip=ip2long(mask[1])) >= 0) ){
+        var freedom = Math.pow(2, 32 - parseInt(mask[2]));
+        return (long_ip > base_ip) && (long_ip < base_ip + freedom - 1);
+    }
+    else return false;
+}
+
+function ValidateIPaddress(ipaddress){
+    var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if(ipaddress.match(ipformat)){
+        return true;
+    }
+    return false;
+}  
+
+/**
+ * Move element of array from src to dst.
+ * @param {type} arr
+ * @param {type} fromIndex
+ * @param {type} toIndex
+ * @returns {Array|arraymove.arr}
+ */
+function arraymove(arr, fromIndex, toIndex) {
+    var element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+    return arr;
+}
