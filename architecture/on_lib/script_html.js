f3dec934ec44ccd6de661009d51aa81065b17a1f
Josep <jbatalle3@gmail.com>
2014-10-03 12:15:18 +0200

    Stencil added. Draggable elements using JQueryUI. New javascript file in order to interact with the html

---
+++ architecture/on_lib/script_html.js
@@ -0,0 +1,65 @@
+$(function () {
+    $(".netEl-drag").draggable({
+        helper: "clone"
+    }); //Network elements draggable
+    $("#graph").droppable({
+        drop: function (event, ui) {
+            x = ui.helper.clone();
+		ui.helper.remove();
+		x.draggable({
+                	helper: 'original',
+                	cursor: 'move',
+                //containment: '#droppable',
+	                tolerance: 'fit',
+                	drop: function (event, ui) {
+	                    $(ui.draggable).remove();
+	        	}
+		});
+            var nodeType = ui.draggable.attr("id"),//select the id of the element
+                divPos = {},
+                $div = $("#graph"),
+                e = window.event;
+
+            divPos = {//position where the element is dropped
+                x: e.pageX - $div.offset().left,
+                y: e.pageY - $div.offset().top
+            };
+
+            createElement(nodeType, divPos);
+            //$("#"+nodeType").remove();
+        }
+
+    });
+});
+
+function createElement(type, divPos) {
+    console.log("Create element " + type + " " + divPos);
+    switch (type) {
+    case "ofSwitch":
+        createofSwitch(divPos);
+        break;
+    case "ofController":
+        createofController(divPos);
+        break;
+    case "host":
+        createHost(divPos);
+        break;
+    case "laptop":
+        createLaptop(divPos);
+        break;
+    }
+}
+
+function createofSwitch(divPos) {
+    ofSwitch.prototype = new NetworkElement();
+    var name = "ofSw" + graph.getNodes().length;
+    var ofSw = new ofSwitch(name);
+    ofSw.id = name;
+    ofSw.setX(divPos.x);
+    ofSw.setY(divPos.y);
+    graph.addNodewithData(ofSw);
+}
+
+function createStencil(){
+    
+}
